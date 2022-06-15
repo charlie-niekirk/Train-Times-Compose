574e93aa713c9b4e12cf4770a26e620d59f95980
charlie-niekirk <chaz759@protonmail.com>
2022-06-15 19:27:23 +0100

    Removed unused code

--- app/src/main/java/me/cniekirk/traintimes/domain/usecase/GetDepartureBoardUseCaseImpl.kt
+++ app/src/main/java/me/cniekirk/traintimes/domain/usecase/GetDepartureBoardUseCaseImpl.kt
@@ -38,14 +38,3 @@ class GetDepartureBoardUseCaseImpl @Inject constructor(
         }
     }
 }
-
-inline fun <T> MutableList<T>.mapInPlace(mutator: (T)->T) {
-    val iterate = this.listIterator()
-    while (iterate.hasNext()) {
-        val oldValue = iterate.next()
-        val newValue = mutator(oldValue)
-        if (newValue !== oldValue) {
-            iterate.set(newValue)
-        }
-    }
-}
